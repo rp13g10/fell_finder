f37273ef78c564e235c2e5ea614f9334c1643561
Ross Piper <rp-webservices@outlook.com>
2024-06-21 10:02:20 +0100

    Populate setup.py  - Update requirements

---
+++ setup.py
@@ -0,0 +1,28 @@
+"""Setup configuration for the relevation package"""
+
+from setuptools import setup
+
+setup(
+    name="fell_finder",
+    version="0.1.0",
+    where="src",
+    include=["fell_finder"],
+    install_requires=[
+        "polars",
+        "pyspark",
+        "pandas",
+        "pyarrow",
+        "numpy",
+        "tqdm",
+        "pytest",
+        "pytest-cov",
+        "pytest-xdist",
+        "rasterio",
+        "bng-latlon",
+        "networkx",
+        "geopy",
+        "thefuzz",
+        "plotly",
+        "dash",
+    ],
+)
