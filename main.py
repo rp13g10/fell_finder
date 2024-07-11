bca340f068de6d1e940c37eeede16fd2a56862c7
Ross Piper <rp-webservices@outlook.com>
2024-07-11 21:06:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from pyspark.sql import SparkSession
 
 from fell_finder.ingestion import LidarLoader, OsmLoader
 from fell_finder.enrichment import GraphEnricher
-from fell_finder.contraction.graph_contractor import GraphContractor
+from fell_finder.contraction import GraphContractor
+from fell_finder.selection import Selector
+
+from fell_finder.containers.routes import RouteConfig
 
 DATA_DIR = "/home/ross/repos/fell_finder/data"
 
@@ -38,3 +41,17 @@ del graph_enricher
 graph_contractor = GraphContractor(DATA_DIR, spark)
 graph_contractor.contract()
 del graph_contractor
+
+spark.stop()
+
+config = RouteConfig(
+    start_lat=50.9690528,
+    start_lon=-1.3832098,
+    target_distance=10000.0,
+    tolerance=0.05,
+    route_mode="hilly",
+    max_candidates=512,
+)
+
+selector = Selector(config, DATA_DIR)
+start_inx, graph = selector.create_graph()
