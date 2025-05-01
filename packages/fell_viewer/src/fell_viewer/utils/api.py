fa674974f3a7ecaf363b2409068e5df81227b49c
Ross Piper <rp-webservices@outlook.com>
2025-05-01 08:17:54 +0100

    Adjust logic for setting number of candidates

--- packages/fell_viewer/src/fell_viewer/utils/api.py
+++ packages/fell_viewer/src/fell_viewer/utils/api.py
@@ -2,6 +2,7 @@
 fell_finder"""
 
 import json
+import os
 from dataclasses import fields
 
 import requests
@@ -28,6 +29,25 @@ def _gen_query_url(config: RouteConfig) -> str:
     return url
 
 
+def _get_max_candidates(config: RouteConfig) -> int:
+    # TODO: Move this into rust code, set based on graph size rather than
+    #       requested distance
+
+    usr_dist_km = config.target_distance // 1000
+
+    min_cands = 128
+    max_cands = int(os.environ.get("FF_MAX_CANDS", "8192")) // 2
+    increment = 128
+
+    cands = int(usr_dist_km * increment)
+
+    if cands < min_cands:
+        return min_cands
+    if cands > max_cands:
+        return max_cands
+    return cands
+
+
 def get_user_requested_route(config: RouteConfig) -> list[Route]:
     """Based on the user-provided configuration, generate routes which match
     their requirements and return them for use in the webapp.
@@ -40,9 +60,12 @@ def get_user_requested_route(config: RouteConfig) -> list[Route]:
 
     """
 
+    max_candidates = _get_max_candidates(config)
+    abs_max_candidates = int(os.environ.get("FF_MAX_CANDS", "8192"))
+
     generated = []
-    for mult in [1, 2, 2, 2]:
-        config.max_candidates *= mult
+    while not generated:
+        config.max_candidates = max_candidates
 
         url = _gen_query_url(config)
 
@@ -55,7 +78,9 @@ def get_user_requested_route(config: RouteConfig) -> list[Route]:
         for route in raw_routes:
             generated.append(Route.from_api_response(route))
 
-        if generated:
+        if max_candidates >= abs_max_candidates:
             break
+
+        max_candidates = min(abs_max_candidates, max_candidates * 2)
 
     return generated
