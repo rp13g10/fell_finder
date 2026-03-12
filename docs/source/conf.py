b5ceba574bf4591d23ed0b4b48b241e4a40802a6
Ross Piper <rp-webservices@outlook.com>
2026-03-12 18:45:46 +0000

    Populate changelog

---
+++ docs/source/conf.py
@@ -0,0 +1,27 @@
+"""Configuration file for the Sphinx documentation builder.
+
+For the full list of built-in configuration values, see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Fell Finder"
+copyright = "2026, Ross Piper"  # noqa: A001
+author = "Ross Piper"
+release = "0.1.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
